064b975f82c824a91cd95594af01e9d292cb50b1
Abhishek Jha <abhishek.jha001996@gmail.com>
2023-09-14 07:48:20 +0530

    newrepo

---
+++ matrixlab/matrixlab/src/Components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../Styles/Home.css"
+import Sidebar from './Sidebar'
+
+const Home = () => {
+  return (
+    <div id='Hcontainer'>
+    <Sidebar />
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
---
+++ matrixlab/matrixlab/src/Components/Sidebar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import "../Styles/Sidebar.css"
+const Sidebar = () => {
+  return (
+    <div id='Scontainer'>
+
+    <div>
+
+    <div className='Sbox'>
+      <img src='' alt='logo' />
+      <p>NFTify</p>
+    </div>
+    <div className='Sbox'></div>
+    <div className='Sbox'></div>
+
+    </div>
+
+
+    <div>
+
+    </div>
+
+
+
+
+
+
+
+    <div id='Sfotter' >
+
+    </div>
+    </div>
+  )
+}
+
+export default Sidebar
